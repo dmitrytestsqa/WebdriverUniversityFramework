d859b8ccebe9ade335aa42c45b51708b7e464249
Dmitry Antonov <antonovdmitry90@gmail.com>
2021-06-11 00:02:42 +0200

    add path to the chromedriver

--- WebdriverUniversityFramework/src/main/java/utils/Constant.java
+++ WebdriverUniversityFramework/src/main/java/utils/Constant.java
@@ -3,7 +3,8 @@ package utils;
 public class Constant {
 
 	public final static String CONFIG_PROPERTIES_DERICTORY = "properties\\config.properties";
-	public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\WebdriverUniversityFramework\\recources\\other\\chromedriver.exe";
+	//public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\WebdriverUniversityFramework\\recources\\other\\chromedriver.exe";
+	public final static String CHROME_DRIVER_DIRECTORY = "C:\\Windows\\System32\\config\\systemprofile\\AppData\\Local\\Jenkins\\.jenkins\\workspace\\Cucumber_project\\WebdriverUniversityFramework\\src\\test\\java\\WebdriverUniversityFramework\\recources\\other";
 	public final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\WebdriverUniversityFramework\\recources\\other\\geckodriver.exe";
 	public final static String IE_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\test\\java\\WebdriverUniversityFramework\\recources\\other\\IEDriverServer.exe";
 	
